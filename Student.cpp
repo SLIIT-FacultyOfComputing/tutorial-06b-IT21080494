c8431d86af11a6abf0964c96e5af1351e2988db0
IT210804941 <88463588+IT21080494@users.noreply.github.com>
2021-08-25 19:16:12 +0000

    Totorial 6 completed

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,16 @@
 #include "Student.h"
 #include <iostream>
-
+#include<string.h>
+using namespace std;
 // Assign studentId and name
-Student::assignDetails() {
+void Student::assignDetails(int student_Id, char name_[20]) {
+  studentId=student_Id;
+  strcpy(name,name_);
   
 }
 
 // Display StudentId and Name
-Student::display() {
-  
+void Student::display() {
+  cout<<"StudentId "<<studentId<<endl;
+  cout<<"Name "<<name<<endl; 
 }
